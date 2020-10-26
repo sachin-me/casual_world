fb851cd67af251c93b5322f541aa27077553bd86
Rajesh Mule <rajeshsmule@gmail.com>
2020-10-26 12:24:07 +0530

    Signup and login is fixed

--- client/src/components/CommonComponents/index.js
+++ client/src/components/CommonComponents/index.js
@@ -1,42 +1,40 @@
 import React from 'react';
 
-import helperFunctions from "../../utility";
-let {validateEmail, validatePassword, toProperCase} = helperFunctions;
+import helperFunctions from '../../utility';
+let { validateEmail, validatePassword, toProperCase } = helperFunctions;
+import { Button } from 'react-bootstrap';
 
 const commonComponents = {
-	InputBox: (props) => {
-		const {name, type, placeholder, handleChange, onBlur} = props;
-		return (
-			<div className="field">
-				<label className="label">{toProperCase(name)}</label>
-				<div className="control">
-					<input className="input" type={type} name={name} placeholder={placeholder} onChange={handleChange} onBlur={onBlur}/>
-				</div>
-			</div>
-		)
-	},
+  InputBox: (props) => {
+    const { name, type, placeholder, handleChange, onBlur } = props;
+    return (
+      <div className="form-group">
+        <label>{toProperCase(name)}</label>
+        <input
+          className="form-control"
+          type={type}
+          name={name}
+          placeholder={placeholder}
+          onChange={handleChange}
+          onBlur={onBlur}
+        />
+      </div>
+    );
+  },
 
-	Message: (props) => {
-		const { message, error} = props;
-	  return (
-	    <div className="signup-status center has-text-danger">
-	      { message || error }
-	    </div>
-	  )
-	},
+  Message: (props) => {
+    const { message, error } = props;
+    return (
+      <div className="signup-status center has-text-danger">
+        {message || error}
+      </div>
+    );
+  },
 
-	SubmitButton: (props) => {
-	  const { text } = props;
-	  return (  
-	    <div className="field">
-	      <p className="control">
-	        <button type="submit" className="button is-primary">
-	          { text }
-	        </button>
-	      </p>
-	    </div>
-	  )
-	}
-}
+  SubmitButton: (props) => {
+    const { text } = props;
+    return <Button type="submit">{text}</Button>;
+  },
+};
 
 export default commonComponents;
