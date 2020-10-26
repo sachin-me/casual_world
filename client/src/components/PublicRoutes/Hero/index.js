c46dc2e1312ad51511a0fdfcda0709382dba67c4
Rajesh Mule <rajeshsmule@gmail.com>
2020-10-26 09:56:59 +0530

    footer is fixed

--- client/src/components/PublicRoutes/Hero/index.js
+++ client/src/components/PublicRoutes/Hero/index.js
@@ -1,37 +1,38 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Footer from './Footer';
 import banner from '../../../images/banner.svg';
+import { Container, Row, Col, Button, Image } from 'react-bootstrap';
 
 class Hero extends Component {
   render() {
     return (
       <>
-        <section className="hero is-medium is-light is-bold">
-          <div className="hero-body">
-            <div className="container columns">
-              <div className="column">
-                <h1 className="title">
+        <Container>
+          <Row>
+            <Col>
+              <div className="mt-5">
+                <h1>
                   It lets you work more collaboratively and get more done.
                 </h1>
-                <h2 className="subtitle">
+                <p>
                   It’s boards, lists, and cards enable you to organize and
                   prioritize your projects in a fun, flexible, and rewarding
                   way.
-                </h2>
-                <Link to="/signup" className="signup-btn">
-                  <button className="button is-link">Sign up</button>
+                </p>
+                <Link to="/signup">
+                  <Button>Sign up</Button>
                 </Link>
               </div>
-              <div className="column">
-                <img className="image is-256x256" src={banner} />
-              </div>
-            </div>
-          </div>
-          <div>
-            <Footer />
-          </div>
-        </section>
+            </Col>
+            <Col>
+              <Image src={banner} />
+            </Col>
+          </Row>
+        </Container>
+        <div>
+          <Footer />
+        </div>
       </>
     );
   }
